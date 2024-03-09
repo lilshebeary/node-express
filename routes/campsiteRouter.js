230d688b0a22196ae0389bedc844d2d5b37c2d40
lilshebeary <cirijaye@gmail.com>
2024-03-09 08:18:56 -0800

    workshop 1 task 1

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -21,4 +21,29 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
+campsiteRouter.route('/:campsiteId')
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plaing');
+        next();
+    })
+    .get((req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+})
+
+    .post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+})
+
+    .put((req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
+})
+
+    .delete('/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite with id: ${req.params.campsiteId}`);
+});
+
 module.exports = campsiteRouter;
